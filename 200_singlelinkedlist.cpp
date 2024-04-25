6b24a3df23b3ac2ddf1ba2a9de9abec0563017a1
Refinaldii <refinaldi9786@gmail.com>
2024-04-25 10:32:33 +0700

    Menambahkan fungsi searchData() untuk mencari data dalam linked list

--- 200_singlelinkedlist.cpp
+++ 200_singlelinkedlist.cpp
@@ -112,9 +112,23 @@ void searchData() {
         system("cls");
         return;
     }
+    else {
+        int nim;
+        cout << "Masukan NIM: ";
+        cin >> nim;
+        Node* currentNode = START;
+        while (currentNode != NULL) {
+            if (currentNode->noMhs == nim) {
+                cout << "NIM: " << currentNode->noMhs << ", Nama: " << currentNode->name << endl;
+                return;
+            }
+            currentNode = currentNode->next;
+        }
+        cout << "Data tidak ditemukan" << endl;
+    }
 }
-int main()
-{
-    std::cout << "Hello World!\n";
+int main() {
+
 }
+
 
